b214fec36f244122420a93197b289c7fb3c2f7f6
Марина Русланівна Власенко <maryna.vlasenko@infotech.gov.ua>
2023-08-04 17:16:12 +0300

    Add methods to check the booked appointment data

--- profile/pages/page_appointment.py
+++ profile/pages/page_appointment.py
@@ -1,4 +1,3 @@
-import datetime
 import json
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
@@ -19,7 +18,6 @@ class PageAppointment:
         self.hospital_readmission_loc = (By.ID, 'chk_hospotal_readmission')
         self.healthcare_program_xpath = '//input[@id="radio_program_{}"]'
         self.visit_date_loc = (By.ID, 'txt_visit_date')
-        self.datepicker_loc = (By.CSS_SELECTOR, '.datepicker')
         self.comment_loc = (By.ID, 'txt_comment')
         self.book_appointment_loc = (By.ID, 'btn-book-appointment')
         self.confirmation_loc = (By.XPATH, '//section[@id="summary"]//h2')
@@ -48,11 +46,7 @@ class PageAppointment:
     def choose_visit_date(self, date: str) -> None:
         date_field = self.driver.find_element(*self.visit_date_loc)
         date_field.clear()
-        if date == 'current day':
-            current_date = datetime.datetime.now().strftime("%d/%m/%Y")
-            date_field.send_keys(current_date)
-        else:
-            date_field.send_keys(date)
+        date_field.send_keys(date)
 
     def set_comment(self, comment: str) -> None:
         comment_field = self.driver.find_element(*self.comment_loc)
@@ -62,24 +56,29 @@ class PageAppointment:
         submit_btn = self.driver.find_element(*self.book_appointment_loc)
         submit_btn.click()
 
-    def set_appointment_data(self, case_name: str) -> None:
+    @staticmethod
+    def read_appointment_data(case_name: str) -> dict:
         with open('../../data/appointment.json', 'r') as file:
             data = json.load(file)
         for case in data:
             if case.get('info') == case_name:
-                facility = case['facility']
-                hospital_readmission = case['hospitalReadmission']
-                healthcare_program = case['healthcareProgram']
-                visit_date = case['date']
-                comment = case['comment']
-                break
+                return {
+                    'facility': case['facility'],
+                    'hospital_readmission': case['hospitalReadmission'],
+                    'healthcare_program': case['healthcareProgram'],
+                    'visit_date': case['date'],
+                    'comment': case['comment']
+                }
         else:
             raise ValueError('Data not found in the JSON appointment data.')
-        self.select_facility(facility)
-        self.check_hospital_readmission(hospital_readmission)
-        self.check_healthcare_program(healthcare_program)
-        self.choose_visit_date(visit_date)
-        self.set_comment(comment)
+
+    def set_appointment_data(self, case_name: str) -> None:
+        data = self.read_appointment_data(case_name)
+        self.select_facility(data['facility'])
+        self.check_hospital_readmission(data['hospital_readmission'])
+        self.check_healthcare_program(data['healthcare_program'])
+        self.choose_visit_date(data['visit_date'])
+        self.set_comment(data['comment'])
 
     def get_confirmation_msg(self) -> str:
         confirmation = self.driver.find_element(*self.confirmation_loc)
@@ -109,4 +108,3 @@ class PageAppointment:
         date_error = WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable(self.visit_date_loc))
         error_msg = date_error.get_attribute('validationMessage')
         return error_msg
-
--- profile/tests/test_appointment.py
+++ profile/tests/test_appointment.py
@@ -7,6 +7,7 @@ from profile.pages.page_appointment import PageAppointment
 @pytest.mark.usefixtures('authentication')
 class TestAppointment:
 
+    @pytest.mark.smoke
     def test_make_appointment(self):
         page = PageAppointment(self.driver)
         page.set_appointment_data('appointment without hospital readmission')
@@ -14,21 +15,64 @@ class TestAppointment:
         confirmation_msg = page.get_confirmation_msg()
         assert confirmation_msg == 'Appointment Confirmation'
 
-    def test_make_appointment_with_hosp_readmission(self):
+    def test_appointment_with_hosp_readmission(self):
+        case = 'appointment with hospital readmission'
         page = PageAppointment(self.driver)
-        page.set_appointment_data('appointment with hospital readmission')
+        page.set_appointment_data(case)
         page.submit_appointment()
-        confirmation_msg = page.get_confirmation_msg()
-        assert confirmation_msg == 'Appointment Confirmation'
+        data = page.read_appointment_data(case)
+        fields_to_check = [
+            ('facility', 'get_booked_facility'),
+            ('hospital_readmission', 'get_booked_hosp_readmission'),
+            ('healthcare_program', 'get_booked_program'),
+            ('visit_date', 'get_booked_visit_date'),
+            ('comment', 'get_booked_comment')
+        ]
+        for field, method in fields_to_check:
+            actual_value = getattr(page, method)()
+            expected_value = data[field]
+            assert actual_value == expected_value, \
+                f"{field} mismatch. Expected '{expected_value}', but got '{actual_value}'"
 
-    def test_make_appointment_min(self):
+    def test_appointment_without_hosp_readmission(self):
+        case = 'appointment without hospital readmission'
         page = PageAppointment(self.driver)
-        page.set_appointment_data('appointment with min data')
+        page.set_appointment_data(case)
         page.submit_appointment()
-        confirmation_msg = page.get_confirmation_msg()
-        assert confirmation_msg == 'Appointment Confirmation'
+        data = page.read_appointment_data(case)
+        fields_to_check = [
+            ('facility', 'get_booked_facility'),
+            ('hospital_readmission', 'get_booked_hosp_readmission'),
+            ('healthcare_program', 'get_booked_program'),
+            ('visit_date', 'get_booked_visit_date'),
+            ('comment', 'get_booked_comment')
+        ]
+        for field, method in fields_to_check:
+            actual_value = getattr(page, method)()
+            expected_value = data[field]
+            assert actual_value == expected_value, \
+                f"{field} mismatch. Expected '{expected_value}', but got '{actual_value}'"
 
-    def test_make_appointment_negative(self):
+    def test_appointment_min(self):
+        case = 'appointment with min data'
+        page = PageAppointment(self.driver)
+        page.set_appointment_data(case)
+        page.submit_appointment()
+        data = page.read_appointment_data(case)
+        fields_to_check = [
+            ('facility', 'get_booked_facility'),
+            ('hospital_readmission', 'get_booked_hosp_readmission'),
+            ('healthcare_program', 'get_booked_program'),
+            ('visit_date', 'get_booked_visit_date'),
+            ('comment', 'get_booked_comment')
+        ]
+        for field, method in fields_to_check:
+            actual_value = getattr(page, method)()
+            expected_value = data[field]
+            assert actual_value == expected_value, \
+                f"{field} mismatch. Expected '{expected_value}', but got '{actual_value}'"
+
+    def test_appointment_negative(self):
         page = PageAppointment(self.driver)
         page.set_appointment_data('empty date field')
         page.submit_appointment()
@@ -38,6 +82,3 @@ class TestAppointment:
         expected_url = page.URL
         current_url = self.driver.current_url
         assert expected_url == current_url, f"Expected URL '{expected_url}', but got '{self.driver.current_url}'"
-
-    def test_check_booked_data(self):
-        pass
