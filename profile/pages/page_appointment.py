606336841c6061ba03eb5ad9a64c9d489f748b2f
Марина Русланівна Власенко <maryna.vlasenko@infotech.gov.ua>
2023-08-04 15:00:07 +0300

    Add positive appointment tests and negative test

--- profile/pages/page_appointment.py
+++ profile/pages/page_appointment.py
@@ -6,8 +6,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.select import Select
 
+from config import Config
+
 
 class PageAppointment:
+
+    URL = Config.BASE_URL + '/#appointment'
 
     def __init__(self, driver: WebDriver):
         self.driver = driver
@@ -18,14 +22,19 @@ class PageAppointment:
         self.datepicker_loc = (By.CSS_SELECTOR, '.datepicker')
         self.comment_loc = (By.ID, 'txt_comment')
         self.book_appointment_loc = (By.ID, 'btn-book-appointment')
-        self.confirmation_loc = (By.CSS_SELECTOR, 'p.lead')
+        self.confirmation_loc = (By.XPATH, '//section[@id="summary"]//h2')
+        self.booked_facility_loc = (By.CSS_SELECTOR, 'p#facility')
+        self.booked_hosp_readmission_loc = (By.CSS_SELECTOR, 'p#hospital_readmission')
+        self.booked_program_loc = (By.CSS_SELECTOR, 'p#program')
+        self.booked_visit_date_loc = (By.CSS_SELECTOR, 'p#visit_date')
+        self.booked_comment_loc = (By.CSS_SELECTOR, 'p#comment')
 
     def select_facility(self, value: str) -> None:
         facility_field_select = Select(self.driver.find_element(*self.facility_loc))
         facility_field_select.select_by_value(value)
 
-    def check_hospital_readmission(self, boolean: bool) -> None:
-        if boolean:
+    def check_hospital_readmission(self, data: str) -> None:
+        if data == 'Yes':
             hospital_readmission_field = self.driver.find_element(*self.hospital_readmission_loc)
             if not hospital_readmission_field.is_selected():
                 hospital_readmission_field.click()
@@ -71,3 +80,33 @@ class PageAppointment:
         self.check_healthcare_program(healthcare_program)
         self.choose_visit_date(visit_date)
         self.set_comment(comment)
+
+    def get_confirmation_msg(self) -> str:
+        confirmation = self.driver.find_element(*self.confirmation_loc)
+        return confirmation.text
+
+    def get_booked_facility(self) -> str:
+        facility = self.driver.find_element(*self.booked_facility_loc)
+        return facility.text
+
+    def get_booked_hosp_readmission(self) -> str:
+        hosp_readmission = self.driver.find_element(*self.booked_hosp_readmission_loc)
+        return hosp_readmission.text
+
+    def get_booked_program(self) -> str:
+        program = self.driver.find_element(*self.booked_program_loc)
+        return program.text
+
+    def get_booked_visit_date(self) -> str:
+        visit_date = self.driver.find_element(*self.booked_visit_date_loc)
+        return visit_date.text
+
+    def get_booked_comment(self) -> str:
+        comment = self.driver.find_element(*self.booked_comment_loc)
+        return comment.text
+
+    def get_date_error_msg(self) -> str:
+        date_error = WebDriverWait(self.driver, 5).until(ec.element_to_be_clickable(self.visit_date_loc))
+        error_msg = date_error.get_attribute('validationMessage')
+        return error_msg
+
--- profile/tests/test_appointment.py
+++ profile/tests/test_appointment.py
@@ -11,10 +11,33 @@ class TestAppointment:
         page = PageAppointment(self.driver)
         page.set_appointment_data('appointment without hospital readmission')
         page.submit_appointment()
-        # дописати assert
+        confirmation_msg = page.get_confirmation_msg()
+        assert confirmation_msg == 'Appointment Confirmation'
 
-    def test_appointment_min(self):
-        pass
+    def test_make_appointment_with_hosp_readmission(self):
+        page = PageAppointment(self.driver)
+        page.set_appointment_data('appointment with hospital readmission')
+        page.submit_appointment()
+        confirmation_msg = page.get_confirmation_msg()
+        assert confirmation_msg == 'Appointment Confirmation'
 
-    def test_appointment_negative(self):
+    def test_make_appointment_min(self):
+        page = PageAppointment(self.driver)
+        page.set_appointment_data('appointment with min data')
+        page.submit_appointment()
+        confirmation_msg = page.get_confirmation_msg()
+        assert confirmation_msg == 'Appointment Confirmation'
+
+    def test_make_appointment_negative(self):
+        page = PageAppointment(self.driver)
+        page.set_appointment_data('empty date field')
+        page.submit_appointment()
+        expected_error_messages = ["Заповніть це поле.", "Please fill out this field.", "Заполните это поле."]
+        error_msg = page.get_date_error_msg()
+        assert error_msg in expected_error_messages, f"Expected one of {expected_error_messages}, but got '{error_msg}'"
+        expected_url = page.URL
+        current_url = self.driver.current_url
+        assert expected_url == current_url, f"Expected URL '{expected_url}', but got '{self.driver.current_url}'"
+
+    def test_check_booked_data(self):
         pass
